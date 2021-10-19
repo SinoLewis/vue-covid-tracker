37c9ca23c4bbebc354bc6a20de446937a7104c63
Lewis West <lewis@reiner.KushUps>
2021-10-19 19:56:20 +0300

    💉 Tracks a COVID-19 Api oh health stats around the globe

---
+++ src/router/index.js
@@ -0,0 +1,23 @@
+import { createRouter, createWebHistory } from 'vue-router'
+import Home from '../views/Home.vue'
+import Test from '../views/Test.vue'
+
+const routes = [
+  {
+    path: '/',
+    name: 'Home',
+    component: Home
+  },
+  {
+    path: '/test',
+    name: 'Test',
+    component: Test
+  }
+]
+
+const router = createRouter({
+  history: createWebHistory(process.env.BASE_URL),
+  routes
+})
+
+export default router
---
+++ src/views/Test.vue
@@ -0,0 +1,25 @@
+<template>
+  <div>
+    <!--Content wrapper-->
+    <div class="flex bg-blue-800 ">
+      <nav>
+        <div class="">
+          <button><i class="fas fa-bars"></i></button>
+          <button><i class="fab fa-youtube"></i></button>
+          <h1>YouTube</h1>
+        </div>
+        <div><!--Search button--></div>
+        <div class="">
+          <button><i class="fas fa-video"></i></button>
+          <button><i class="fas fa-user-circle"></i></button>
+          <button><i class="fas fa-border-none"></i></button>
+          <button><i class="fas fa-bell"></i></button>
+        </div>
+      </nav>
+    </div>
+  </div>
+</template>
+
+<script>
+export default {};
+</script>
